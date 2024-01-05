1134112cba8a8a4eaf277ab502ad37cbe74b3657
ECU44 <91726557+ECU44@users.noreply.github.com>
2024-01-05 17:20:22 +0300

    sasaas

---
+++ src/models/company.ts
@@ -0,0 +1,50 @@
+import sequelize from "../db/sequelize";
+import { DataTypes } from "sequelize";
+import Sequelize from "sequelize";
+
+const Company = sequelize.define('company', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  phoneNum: Sequelize.STRING,
+  mail: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  country: {
+    type: Sequelize.STRING,
+  },
+  address: {
+    type: Sequelize.STRING,
+  },
+  employeeNum: {
+    type: Sequelize.INTEGER,
+  },
+  sector: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+});
+
+module.exports = Company;
+
+Company
