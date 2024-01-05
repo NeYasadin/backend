ea389bc147e41b3baeefa820354501945ee11cd4
Mustafa Er <91684719+ER-Mustafa@users.noreply.github.com>
2024-01-05 18:16:58 +0300

    complaint references.

--- src/models/complaint.ts
+++ src/models/complaint.ts
@@ -1,5 +1,7 @@
 import sequelize from "../db/sequelize";
 import { DataTypes } from "sequelize";
+import Company from "./company";
+import Customer from "./customer";
 
 const Complaint = sequelize.define("complaint", {
   id: {
@@ -16,6 +18,10 @@ const Complaint = sequelize.define("complaint", {
   },
   companyId: {
     type: DataTypes.INTEGER,
+    references: {
+      model: Company,
+      key: "id",
+    },
   },
   meToo: {
     type: DataTypes.INTEGER,
@@ -27,6 +33,10 @@ const Complaint = sequelize.define("complaint", {
   },
   customerId: {
     type: DataTypes.INTEGER,
+    references: {
+      model: Customer,
+      key: "id",
+    },
   },
   priorityLevel: {
     type: DataTypes.INTEGER,
@@ -35,6 +45,10 @@ const Complaint = sequelize.define("complaint", {
   isFinished: {
     type: DataTypes.BOOLEAN,
     allowNull: false,
+  },
+  solutionIds: {
+    type: DataTypes.ARRAY(DataTypes.INTEGER),
+    allowNull: true,
   },
 });
 
