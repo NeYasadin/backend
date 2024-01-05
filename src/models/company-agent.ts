5701f4bf74dbfc03a4768dd7b15af63b4d7e23f6
abahadircelik <acelik20@ku.edu.tr>
2024-01-05 17:54:10 +0300

    complaint and company-agent models added.

---
+++ src/models/company-agent.ts
@@ -0,0 +1,37 @@
+import sequelize from "../db/sequelize";
+import { DataTypes } from "sequelize";
+
+const CompanyAgent = sequelize.define('company_agent', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mail: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  companyId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  subscriptionId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+export default CompanyAgent;
---
+++ src/models/complaint.ts
@@ -0,0 +1,41 @@
+import sequelize from "../db/sequelize"
+import { DataTypes } from "sequelize";
+
+const Complaint = sequelize.define('complaint', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  solutionRating: {
+    type: DataTypes.INTEGER,
+    defaultValue: null
+  },
+  complaintRating: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  companyId: {
+    type: DataTypes.INTEGER,
+  },
+  meToo: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  misleading: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  customerId: {
+    type: DataTypes.INTEGER,
+  },
+  priorityLevel: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  isFinished: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
+});
+
+module.exports = Complaint;
